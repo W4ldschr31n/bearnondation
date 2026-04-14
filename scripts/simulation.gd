3bea9fa0286087ab8f1c66a71ed8fadfc82d7b3b
Esma-NicolasR <186063893+ESMA-NicolasR@users.noreply.github.com>
2026-04-14 13:47:52 +0200

    [ADD] tilemap

--- scripts/simulation.gd
+++ scripts/simulation.gd
@@ -1,60 +1,6 @@
 class_name Simulation
 extends Node
 
-
-class Tile:
-	# Coordinates
-	var x : int
-	var y : int
-	# Type of tile
-	var height : int
-	var is_source : bool
-	# Water filling
-	var max_filling : int
-	var current_filling : int
-	# Water flowing
-	var max_flow : int
-	var current_flow : int
-	
-	func _init(height, is_source, filling, flow):
-		self.height = height
-		self.is_source = is_source
-		self.max_filling = filling
-		self.max_flow = flow
-		if(is_source):
-			self.current_filling = filling
-			self.current_flow = flow
-	
-	func is_filled() -> bool:
-		return max_filling <= current_filling
-	
-	func is_flowing() -> bool:
-		return max_flow <= current_flow
-		
-	func send_flow(amount : int) -> int:
-		if(!is_filled()):
-			var fill_qty = min(max_filling - current_filling, amount)
-			amount -= fill_qty
-		elif(!is_flowing()):
-			var flow_qty = min(max_flow - current_flow, amount)
-			amount -= flow_qty
-		return amount
-	
-	static func NewLand():
-		return Tile.new(0, false, 10, 10)
-	
-	static func NewHill():
-		return Tile.new(1, false, 15, 15)
-		
-	static func NewForest():
-		return Tile.new(2, false, 20, 20)
-		
-	static func NewMountain():
-		return Tile.new(3, false, 25, 25)
-		
-	static func NewSource(flow=100):
-		return Tile.new(4, true, 50, flow)
-
 var tiles : Array[Array]
 var sources : Array
 var width: int
@@ -164,6 +110,7 @@ func print_board():
 	print_tiles_flow()
 	print("\nIndex")
 	print_tiles_index()
+
 
 func trickle_down(source: Tile):
 	var flow_to_unload = source.current_flow
